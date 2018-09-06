016ee47b929ff0c9ffe169ac2d59b3628142e13a
shobull <lubos.palisek@ataccama.com>
2018-09-06 22:18:38 +0200

    Route for non api calls return index.html from client

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var frontendRouter = require('./routes/frontend');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,16 +20,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve any static files
+//app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use(/^(?!\/api\/).*/, frontendRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
